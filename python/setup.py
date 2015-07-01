88ef388568c6886ee41af1790666ae2120653ecb
Sean Macdonald <sean@crazyhorsecoding.com>
2015-07-01 00:18:22 -0400

    fixed obscure bug in python client

---
+++ python/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+	name = "sizeify",
+	packages = ["sizeify"],
+	version = "1.0.7",
+	description = "official client for sizeify, the image resizing and hosting service",
+	author = "Sean Macdonald",
+	author_email = "sean@crazyhorsecoding.com",
+	url = "http://sizeifyb.sjc.io",
+	download_url = "https://github.com/stjosephcontent/sizeify-client/archive/v1.0.7.zip",
+	keywords = ["image", "resize", "cdn"],
+	classifiers = [
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3",
+		"Development Status :: 4 - Beta",
+		"Environment :: Other Environment",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+		"Operating System :: OS Independent",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Multimedia :: Graphics :: Graphics Conversion"
+	]
+);
